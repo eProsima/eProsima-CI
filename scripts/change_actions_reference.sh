a76eda7fd2ca721a9985ca4d5ea5322337e57a8b
Ricardo González <correoricky@gmail.com>
2023-10-23 15:10:00 +0200

    Several new actions (#37)
    * Refs #19657. Add action to install ccache
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    * Refs #19657. Add action to install cmake
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    * Refs #19657. Install default colcon mixin repository
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    * Refs #19657. Setup python action
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    * Refs #19657. New script to change references
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    * Refs #19657. Return to main branch
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    * Refs #19657. Update README
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    * Refs #19657. Apply suggestions
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    ---------
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

---
+++ scripts/change_actions_reference.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+red='\E[1;31m'
+yellow='\E[1;33m'
+textreset='\E[1;0m'
+
+current_dir=$(git rev-parse --show-toplevel)
+
+if [[ ! "$(pwd -P)" -ef "$current_dir" ]]; then
+    echo -e "${red}This script must be executed in the repository root directory.${textreset}"
+    exit -1
+fi
+
+if [[ $# -ne 2 ]]; then
+    echo -e "${red}Invalid number of arguments.${textreset}"
+    echo "Usage: ./scripts/change_actions_reference.sh [current_reference] [new_reference]"
+    echo "Example: ./scripts/change_actions_reference.sh v0.2 feature/my-branch"
+fi
+
+current_ref=$1
+new_ref=$2
+
+echo -e "Changing current reference ${yellow}@$current_ref${textreset} to ${yellow}@$new_ref${textreset}."
+
+git grep -lz "@$current_ref" -- :^external :^.github :^README.md | xargs -0 sed -i "s+@$current_ref+@$new_ref+g"
